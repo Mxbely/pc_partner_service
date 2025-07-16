4ae525dbd61f262e8e8a3e2595413442e0447dda
Mxbely <mxbely98@gmail.com>
2025-07-16 18:08:18 +0300

    set simple template

--- search_service/main.py
+++ search_service/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+templates = Jinja2Templates(directory="/app/templates")
+print(f"Current dir {os.getcwd()}")
+
+@app.get("/", response_class=HTMLResponse)
+def search(request: Request, query: str = ""):
+    return templates.TemplateResponse(request=request, name="search_list.html", context={"request": request, "query": query})
